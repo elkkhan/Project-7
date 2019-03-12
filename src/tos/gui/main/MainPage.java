ce4c46608763c71ffda74d174312798cbd1cf8a3
TahaFurkanNurdag <tahafurkannurdag@gmail.com>
2019-03-12 21:29:09 +0100

    LocationError Fixed

--- src/tos/gui/main/MainPage.java
+++ src/tos/gui/main/MainPage.java
@@ -1,8 +1,9 @@
 package tos.gui.main;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.TimeZone;
-import tos.common.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,12 +17,15 @@ public class MainPage extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+        URL url1 = new File("src\\tos\\gui\\view\\MainPage.fxml").toURL();
+        URL url2 = new File("src\\tos\\gui\\view\\newstyle.css").toURL();
 		try {
+
             TimeZone.setDefault(TimeZone.getTimeZone("Europe/Budapest"));
 
-            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("../view/MainPage.fxml"));
+            AnchorPane root = (AnchorPane) FXMLLoader.load(url1);
             Scene scene = new Scene(root);
-            scene.getStylesheets().add(getClass().getResource("../view/newstyle.css").toExternalForm());
+            scene.getStylesheets().add(url2.toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.setResizable(true);
             primaryStage.setTitle("Receip");
@@ -31,13 +35,14 @@ public class MainPage extends Application{
             e.printStackTrace();
         }
 	}
-    /*
+
+
 	 public static void main(String[] args) {
 	        launch(args);
 	        
 	    }
 
-	    */
+
 	
 	
 }
