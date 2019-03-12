1292373d916e616109dc87b9ca685d3c506da5c9
TahaFurkanNurdag <tahafurkannurdag@gmail.com>
2019-03-12 17:18:23 +0100

    Model is created , Controller is created and controller skeleton functions created , FXML's are ready to use . Authentication skeleton is ready .

---
+++ src/tos/gui/controller/AdminController.java
@@ -0,0 +1,17 @@
+package tos.gui.controller;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class AdminController implements Initializable {
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
---
+++ src/tos/gui/main/MainPage.java
@@ -0,0 +1,46 @@
+package tos.gui.main;
+
+import java.io.IOException;
+import java.util.TimeZone;
+import tos.common.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import tos.common.api.entities.Recipe;
+
+public class MainPage extends Application{
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+            TimeZone.setDefault(TimeZone.getTimeZone("Europe/Budapest"));
+
+            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("../view/MainPage.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("../view/newstyle.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(true);
+            primaryStage.setTitle("Receip");
+            primaryStage.show();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	
+		
+	}
+
+    /*
+	 public static void main(String[] args) {
+	        launch(args);
+	        
+	    }
+
+	    */
+	
+	
+}
