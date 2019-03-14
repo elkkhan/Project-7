6aec37c05ee7ba3c8936ce52b04537484f5e2144
DaniiilU <danil@usatenko.com>
2019-03-14 12:14:10 +0100

    Base model fo chat. In the next commit two users will be able to comunicate, and also chat bot will be in there.

---
+++ src/tos/chat/Client/Client.java
@@ -0,0 +1,132 @@
+package tos.chat.Client;
+
+import tos.chat.Connection;
+import tos.chat.ScreenShower;
+import tos.chat.Message;
+import tos.chat.MessageType;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    protected Connection connection;
+
+    private volatile boolean clientConnected = false;
+    Scanner in = new Scanner(System.in);
+    public class SocketThread extends Thread {
+
+        public void run(){
+
+            try {
+                Socket socket = null;
+                socket = new Socket(getServerAddress(),getServerPort());
+                connection = new Connection(socket);
+                clientHandshake();
+                clientLoop();
+            } catch (IOException | ClassNotFoundException e) {
+
+                notifyConnectionStatusChanged(false);
+            }
+
+
+        }
+
+        protected void processIncomingMessage(String message){
+            ScreenShower.writeMessage(message);
+        }
+        protected void informAboutAddingNewUser(String userName){
+            ScreenShower.writeMessage(userName + " has joined the chat.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected){
+            synchronized (Client.this){
+                Client.this.clientConnected=clientConnected;
+                Client.this.notify();
+            }
+        }
+        protected void clientHandshake() throws IOException, ClassNotFoundException{
+            Message m=null;
+
+            while (!clientConnected) {
+                try {
+                    m = connection.receive();
+                } catch (ClassNotFoundException e) {
+                    throw new IOException("Unexpected MessageType");
+                }
+                if (m.getType() == MessageType.NAME_REQUEST) {
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+                }
+                else {
+                    if (m.getType() == MessageType.NAME_ACCEPTED) {
+                        notifyConnectionStatusChanged(true);
+                    }
+                    else throw new IOException("Unexpected MessageType");
+                }
+
+            }
+
+        }
+        protected void clientLoop() throws IOException, ClassNotFoundException{
+            Message m=null;
+            while(true) {
+                m = connection.receive();
+
+                if (m.getType() == MessageType.TEXT) {
+                    processIncomingMessage(m.getData());
+                }
+                else if (m.getType() == MessageType.USER_ADDED) {
+                    informAboutAddingNewUser(m.getData());
+                }
+                else throw new IOException("Unexpected MessageType");
+            }
+        }
+
+    }
+    public static void main(String[] args){
+        Client client = new Client();
+
+        client.run();
+    }
+
+
+
+    protected String getServerAddress(){
+        System.out.print("Input an IP(e.g. localhost): ");
+        String ipAddress= ScreenShower.readString();
+        return ipAddress;
+    }
+    protected int getServerPort(){
+        return 2020;
+    }
+
+    protected String getUserName(){
+        System.out.print("Input your name: ");
+        String username= ScreenShower.readString();
+        return username;
+    }
+
+
+    protected SocketThread getSocketThread(){
+        SocketThread newThread = new SocketThread();
+
+        return newThread;
+    }
+
+    public void run(){
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+
+        try {
+            synchronized (this){
+                wait();
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted");
+            System.exit(1);
+        }
+    }
+
+}
