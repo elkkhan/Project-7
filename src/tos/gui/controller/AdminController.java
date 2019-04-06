cb3fb3ac4fbe820e38c361e1f34f5782c1bb752f
elkhan <elxaneminov99@gmail.com>
2019-04-06 23:38:37 +0200

    merging back the GUI branch

---
+++ src/tos/gui/controller/AdminController.java
@@ -0,0 +1,12 @@
+package tos.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class AdminController implements Initializable {
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+  }
+}
---
+++ src/tos/gui/model/Users.java
@@ -0,0 +1,11 @@
+package tos.gui.model;
+
+import java.util.Date;
+
+public class Users extends Person {
+
+  public Users(String name, String surname, String id, Date age, String email, byte[] salt) {
+    super(name, surname, id, age, email, salt);
+  }
+}
+
